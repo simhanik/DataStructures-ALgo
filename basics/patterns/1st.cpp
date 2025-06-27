31dafb590cef55642ee435e425c6b264d0bad243
simhanik <nikhilsahu0427@gmail.com>
2025-06-27 13:06:22 +0530

    added new pattern codes

--- basics/patterns/1st.cpp
+++ basics/patterns/1st.cpp
@@ -35,6 +35,62 @@ void print4(int n){
     }
 }
 
+void print5(int n){
+    for(int i = n; i>=0 ; i--){
+        for (int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+}
+
+void print6(int n){
+    for(int i = n; i>=0 ; i--){
+        for (int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void print7(int n){
+    for(int i=0; i<n; i++){
+        // space
+        for(int j = 0; j<n-i-1;j++){
+            cout<<" ";
+        }
+        //star
+        for(int k = 1; k<=2*i+1; k++){
+            cout<<"*";
+        }
+
+        // space
+        for(int j =0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+void print8(int n){
+    for(int i=n; i>0; i--){
+        // space
+        for(int j = 0; j<n-i;j++){
+            cout<<" ";
+        }
+        //star
+        for(int k = 1; k<=2*i-1; k++){
+            cout<<"*";
+        }
+
+        // space
+        for(int j =0;j<n-i;j++){
+            cout<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
 int main(){
     int t;
     cout<<"enter t: ";
@@ -43,7 +99,7 @@ int main(){
         int n;
         cout<<"enter n: ";
         cin>>n;
-        print4(n);
+        print8(n);
     }
     return 0;
 }
@@ -100,4 +156,35 @@ enter n: 5
 3 3 3
 4 4 4 4
 5 5 5 5 5
+
+print5(n)
+enter t: 2
+enter n: 3
+* * * 
+* *
+*
+
+enter n: 5
+* * * * *
+* * * *
+* * *
+* *
+*
+
+print6(n)
+enter n: 5
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+
+print7(n)
+enter n: 5
+    *    
+   ***
+  *****
+ *******
+*********
+
 */
