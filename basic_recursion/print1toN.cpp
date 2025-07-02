65a6561d6924f1ca3e9ce712deeab3aa23815e9f
simhanik <nikhilsahu0427@gmail.com>
2025-07-02 17:24:12 +0530

    done by backtracking

--- basic_recursion/print1toN.cpp
+++ basic_recursion/print1toN.cpp
@@ -5,10 +5,19 @@ void nTimes(int i, int n){
     cout<<i<<endl;
     nTimes(i+1,n);
 }
+
+// by Backtracking
+void fun(int i, int  n){
+    if(i<1) return;
+    fun(i-1,n);
+    cout<<i<<endl;
+}
+
 int main(){
     int n;
     cout<<"Enter how many times you want to print: ";
     cin>>n;
-    nTimes(1,n);
+    //nTimes(1,n);
+    fun(n,n);
     return 0;
 }
--- basic_recursion/printNto1.cpp
+++ basic_recursion/printNto1.cpp
@@ -5,10 +5,19 @@ void nTimes(int i, int n){
     cout<<i<<endl;
     nTimes(i-1,n);
 }
+
+//  by Backtracking
+void fun(int i, int  n){
+    if(i>n) return;
+    fun(i+1,n);
+    cout<<i<<endl;
+}
+
 int main(){
     int n;
     cout<<"Enter how many times you want to print: ";
     cin>>n;
-    nTimes(n,n);
+    //nTimes(n,n);
+    fun(1,n);
     return 0;
 }
